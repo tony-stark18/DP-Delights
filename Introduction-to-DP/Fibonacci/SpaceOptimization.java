f19c37ea9dc03764a1b44f77496c62938421aeb6
tony-stark18 <sahoodiptiranjan885@gmail.com>
2024-03-20 22:22:10 +0530

    Nth Fibonacci using Dynamic Programming

---
+++ Introduction-to-DP/Fibonacci/SpaceOptimization.java
@@ -0,0 +1,16 @@
+package Fibonacci;
+public class SpaceOptimization {
+    public static void main(String args[]) {
+        int n=5;
+        
+        int prev2 = 0;
+        int prev = 1;
+        
+        for(int i=2; i<=n; i++){
+            int cur_i = prev2+ prev;
+            prev2 = prev;
+            prev= cur_i;
+        }
+        System.out.println(prev);
+    }
+}
---
+++ Introduction-to-DP/Fibonacci/Tabulation.java
@@ -0,0 +1,17 @@
+package Fibonacci;
+import java.util.Arrays;
+public class Tabulation {
+    public static void main(String args[]) {
+
+        int n=5;
+        int dp[]=new int[n+1];
+        Arrays.fill(dp,-1);
+        dp[0]= 0;
+        dp[1]= 1;
+        
+        for(int i=2; i<=n; i++){
+            dp[i] = dp[i-1]+ dp[i-2];
+        }
+        System.out.println(dp[n]);
+    }
+}
